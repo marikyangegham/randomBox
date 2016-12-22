fdf46f053615b9863a76070bc79ce6507e153a18
gmarikya <gegham.marikyan@monitis.com>
2016-12-22 16:59:42 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+let app = angular.module('randomBox', []);
+app.controller('getRandomUsersController', ['$http', '$scope', function ($http, $scope) {
+    $scope.usersParams = [];
+    let getUsers = function () {
+        let randomValue = Math.floor(Math.random() * 500);
+        $http({method: 'GET', url: 'https://api.github.com/users?since=' + randomValue})
+            .then(function (data) {
+                 addUsers(data.data);
+            }).catch(function (error) {
+            console.log(error);
+        });
+    };
+
+    let addUsers = function (usersData) {
+        usersData.forEach(function (item) {
+            $scope.usersParams.push(item);
+        });
+    };
+
+    let isEnoughUsers = function (usersLastIndex) {
+        return usersLastIndex < 4;
+    };
+
+    $scope.remove = function (id) {
+        let usersDataLastIndex = $scope.usersParams.length - 1;
+        if(isEnoughUsers(usersDataLastIndex)){
+            getUsers();
+        }
+        $scope.usersParams[id] = $scope.usersParams[usersDataLastIndex];
+        $scope.usersParams = $scope.usersParams.slice(0, -1);
+    };
+
+   getUsers();
+}]);
